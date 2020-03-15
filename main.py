9c4780476eefbf4f09a6c5be888c635ad9296a30
TAT527 <47490150+TAT527@users.noreply.github.com>
2020-03-15 16:20:22 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(Lala.dialogue(text))) #ここでオウム返しのメッセージを返します。
+        TextSendMessage("原因の切り分けチェック") #ここでオウム返しのメッセージを返します。
 # ポート番号の設定
 if __name__ == "__main__":
 #    app.run()
