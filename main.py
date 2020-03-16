bd22d5f90ffebe19faee9933a71cea7d8ff31c04
TAT527 <47490150+TAT527@users.noreply.github.com>
2020-03-16 20:23:14 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def handle_message(event):
     	Lala = Unmo('Lala')
     #while True:
         #text = TextMessage
-    response = Lala.dialogue(text=event.message)
+    response = Lala.dialogue(text=message)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=response)) #ここでオウム返しのメッセージを返します。
