b9fddb0f6202b26a8ddfa170f057e4e8877bb760
TAT527 <47490150+TAT527@users.noreply.github.com>
2020-03-22 23:25:26 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def build_prompt(unmo):
  
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.reply_token == "00000000000000000000000000000000":
-        return
+    #if event.reply_token == "00000000000000000000000000000000":
+        #return
     if __name__ == '__main__':
     	Lala = Unmo('Lala')
     #while True:
